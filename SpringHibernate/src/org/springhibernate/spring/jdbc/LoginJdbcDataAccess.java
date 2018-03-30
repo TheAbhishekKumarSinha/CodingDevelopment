34866835b9c554cdd9490afa08fc915567151a76
abhsinha <abhishekkumar.sinha@misys.com>
2018-03-31 01:15:33 +0530

    Committed from office laptop for new webservice

--- SpringHibernate/src/org/springhibernate/spring/jdbc/LoginJdbcDataAccess.java
+++ SpringHibernate/src/org/springhibernate/spring/jdbc/LoginJdbcDataAccess.java
@@ -1,6 +1,5 @@
 package org.springhibernate.spring.jdbc;
 
-
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -15,50 +14,52 @@ import org.springhibernate.spring.jdbc.mappers.UserMapper;
 @Controller
 public class LoginJdbcDataAccess {
 
-	private DataSource dataSource;
-	private JdbcTemplate jdbctemplate;
+    private DataSource dataSource;
+    private JdbcTemplate jdbctemplate;
 
-	public DataSource getDataSource() {
-		return dataSource;
-	}
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		this.jdbctemplate = new JdbcTemplate(dataSource);
-	}
+    public DataSource getDataSource() {
+        return dataSource;
+    }
 
-	public JdbcTemplate getJdbctemplate() {
-		return jdbctemplate;
-	}
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbctemplate = new JdbcTemplate(dataSource);
+    }
 
-	public void setJdbctemplate(JdbcTemplate jdbctemplate) {
-		this.jdbctemplate = jdbctemplate;
-	}
+    public JdbcTemplate getJdbctemplate() {
+        return jdbctemplate;
+    }
 
-	public Users getUser(String userid,String password)throws Exception{
-		String sql="SELECT * FROM USERS WHERE USERID=?";
-		Users user= this.jdbctemplate.queryForObject(sql, new Object[]{userid}, new UserMapper());
-		return user;
-	}
+    public void setJdbctemplate(JdbcTemplate jdbctemplate) {
+        this.jdbctemplate = jdbctemplate;
+    }
 
-	public void updateUser(String[] ColumnName,String[] whereclause,String tableName,String[] columnValue,String[] whereClauseValue){
-		String Sql="UPDATE "+tableName+" SET ";
-		for(int i=0;i<=ColumnName.length-1;i++){
-			Sql=Sql+ ColumnName[i]+"="+"'"+columnValue[i]+"',";
-		}
-		Sql=Sql.substring(0,Sql.length()-1)+" WHERE ";
-		for(int i=0;i<=whereclause.length-1;i++){
-			Sql=Sql+ whereclause[i]+"='"+whereClauseValue[i]+"' AND ";
-		}
-		Sql=Sql.substring(0,Sql.length()-4);
-		this.jdbctemplate.update(Sql);
-		System.out.println(Sql);
-	}
-	public int[] batchUpdate(final List<Users> users){
-		String sql="INSERT INTO USERS(userid,username,password,unsuccessfulLoginAttempts,userLocked,userLockedReason,usercreationdate,lastlogintime) "+
-				"values(?,?,?,?,?,?,?,?)";
-		int[] updatecount=jdbctemplate.batchUpdate(sql, new PreparedStatementSetter(users));
-		return updatecount;
-	}
+    public Users getUser(String userid, String password) throws Exception {
+        String sql = "SELECT * FROM USERS WHERE USERID=?";
+        Users user = this.jdbctemplate.queryForObject(sql, new Object[] { userid }, new UserMapper());
+        return user;
+    }
 
+    public void updateUser(String[] ColumnName, String[] whereclause, String tableName, String[] columnValue,
+            String[] whereClauseValue) {
+        String Sql = "UPDATE " + tableName + " SET ";
+        for (int i = 0; i <= ColumnName.length - 1; i++) {
+            Sql = Sql + ColumnName[i] + "=" + "'" + columnValue[i] + "',";
+        }
+        Sql = Sql.substring(0, Sql.length() - 1) + " WHERE ";
+        for (int i = 0; i <= whereclause.length - 1; i++) {
+            Sql = Sql + whereclause[i] + "='" + whereClauseValue[i] + "' AND ";
+        }
+        Sql = Sql.substring(0, Sql.length() - 4);
+        this.jdbctemplate.update(Sql);
+        System.out.println(Sql);
+    }
+
+    public int[] batchUpdate(final List<Users> users) {
+        String sql = "INSERT INTO BFDBUSR.USERS(userid,username,password,unsuccessfulLoginAttempts,userLocked,userLockedReason,usercreationdate,lastlogintime) "
+                + "values(?,?,?,?,?,?,?,?)";
+        int[] updatecount = jdbctemplate.batchUpdate(sql, new PreparedStatementSetter(users));
+        return updatecount;
+    }
 
 }
